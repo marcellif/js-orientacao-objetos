7d7798896a416a76f264addcd47ec331cf3ca981
Marcelli Pires Faria <marcelli.faria@onidata.com>
2026-03-22 21:51:29 -0300

    criando clase que extende superclasse user

--- Admin.js
+++ Admin.js
@@ -1,1 +1,15 @@
 import User from "./User.js";
+
+class Admin extends User{
+    constructor(nome, email, nascimento, role = "admin", ativo = true){
+        super(nome, email, nascimento, role, ativo)
+    }
+}
+
+
+const novoAdmin =  new Admin("Helio", "helinho@gmail.com", "10/02/2005");
+
+
+console.log(novoAdmin);
+
+console.log(novoAdmin.exibirInfos());
