57aa6b9bedcee9d5297e2f070e164dc58eee802b
Marcelli Pires Faria <marcelli.faria@onidata.com>
2026-03-24 12:35:33 -0300

    adicionando novos métodos a classe admin e herdando de User

--- Admin.js
+++ Admin.js
@@ -4,6 +4,18 @@ class Admin extends User{
     constructor(nome, email, nascimento, role = "admin", ativo = true){
         super(nome, email, nascimento, role, ativo)
     }
+
+    criaCurso(curso){
+        return `O curso ${curso} foi criado!`
+    }
+
+    excluirCurso(curso){
+         return `O curso ${curso} foi excluido!`
+    }
+
+    desativarPerfil(user){
+    return `O perfil de ${user} foi desativado!`
+    }
 }
 
 
@@ -12,4 +24,7 @@ const novoAdmin =  new Admin("Helio", "helinho@gmail.com", "10/02/2005");
 
 console.log(novoAdmin);
 
+
 console.log(novoAdmin.exibirInfos());
+console.log(novoAdmin.excluirCurso('inglês'));
+console.log(novoAdmin.desativarPerfil('Andrea'));
