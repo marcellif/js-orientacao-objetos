9f8adee3f3fc1ead1ed5b0a7ac77c9363e1af067
Marcelli Pires Faria <marcelli.faria@onidata.com>
2026-03-24 20:15:52 -0300

    Classe docente e seus métodos

---
+++ Docente.js
@@ -0,0 +1,21 @@
+import User from "./User.js";
+
+class Docente extends User{
+    constructor(nome, email, nascimento, role = "docente", ativo = true){
+        super(nome, email, nascimento, role, ativo)
+    }
+
+    aprovarOuReprovarEstudante(nome, aprovado){
+        if (aprovado === true){
+            return `o aluno ${nome} foi aprovado`
+        }else{
+             return `o aluno ${nome} foi reprovado`
+        }
+    }
+}
+
+
+const novoDocente =  new Docente("Mirna", "Mirna@gmail.com", "10/02/1988");
+
+console.log (novoDocente.aprovarOuReprovarEstudante('Luan', true))
+console.log (novoDocente.aprovarOuReprovarEstudante('Reinaldo', false))
